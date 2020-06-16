f3eb06a379f255c18d17e04b269c3063515f3dfe
Jenny <jennyqr10110@gmail.com>
2020-06-16 22:23:30 +0200

    Mathclass Korrigieren

--- src/MathTest.java
+++ src/MathTest.java
@@ -1,6 +1,7 @@
 public class MathTest {
     public static void main (String [] args){
         MyMath m1 = new MyMath();
-        System.out.println(m1.arrSumRechnen());
+        int [] tset = {2,6,67,3};
+        System.out.println(m1.arrSumRechnen(tset));
     }
 }
--- src/MyMath.java
+++ src/MyMath.java
@@ -1,14 +1,8 @@
-import java.util.Scanner;
 public class MyMath {
-    Scanner sc = new Scanner(System.in);
-    public int arrSumRechnen (){
-        System.out.println("Wie viele Zahlen wollen Sie eingeben?");
-        int index = sc.nextInt();
-        int [] arr = new int [index];
+    public int arrSumRechnen (int[] integersToSumUp){
         int sum = 0;
-        for (int i = 0;i<index;i++){
-            arr [i] = sc.nextInt();
-            sum += arr[i];
+        for (int i = 0;i<integersToSumUp.length;i++){
+            sum += integersToSumUp [i];
         }
         return(sum);
     }
